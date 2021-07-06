3c5d5d615fb47363c975b4aa4abc070845d32fec
Natalia Davydova <davydovanatalia063@gmail.com>
2021-07-06 13:28:17 +0400

    add functions openCart and closeCart

--- scripts/script.js
+++ scripts/script.js
@@ -1,4 +1,6 @@
-const headerCityButton = document.querySelector('.header__city-button');
+const headerCityButton = document.querySelector('.header__city-button'),
+  subheaderCart = document.querySelector('.subheader__cart'),
+  cartOverlay = document.querySelector('.cart-overlay');
 
 headerCityButton.textContent = localStorage.getItem('lamoda-location') || 'What is your city?'
 
@@ -6,4 +8,18 @@ headerCityButton.addEventListener('click', () => {
   const city = prompt('What is your city');
   headerCityButton.textContent = city;
   localStorage.setItem('lamoda-location', city);
-})
+});
+
+const openCart = () => {
+  cartOverlay.classList.add('cart-overlay-open');
+}
+
+closeCart = (e) => {
+  let target = e.target;
+  if (target.closest('.cart__btn-close') || !target.closest('.cart')) {
+    cartOverlay.classList.remove('cart-overlay-open');
+  }
+}
+
+subheaderCart.addEventListener('click', openCart);
+cartOverlay.addEventListener('click', closeCart);
